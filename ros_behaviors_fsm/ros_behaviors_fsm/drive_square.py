2e52c74e4784d4ace2f6804a1a5105fe30fdd12a
Smart-coder872 <mmiller4328@gmail.com>
2026-09-25 22:47:58 -0400

    Potentially working drive square.py with fsm node

--- ros_behaviors_fsm/ros_behaviors_fsm/drive_square.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/drive_square.py
@@ -1,101 +1,82 @@
-"""
-Draw Square
---------
-This node encapsulates implements a simple time-based approach to driving the
-robot in a square.  The system makes use of a a special ``estop`` topic that
-can trigger the robot to automatically stop when the value is true is received
-on that topic.
-"""
 import rclpy
 from rclpy.node import Node
-from threading import Thread, Event
-from time import sleep
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Bool
-import math
+from std_msgs.msg import String
+from math import pi
 
-class DrawSquare(Node):
-    """A class that implements a node to pilot a robot in a square.
-    """
-
+class DriveSquareSample1(Node):
+    STATE_NAME ='DRIVE_SQUARE'
     def __init__(self):
-        super().__init__('draw_square_with_estop')
-        self.e_stop = Event()
-        # create a thread to handle long-running component
+        super().__init__('drive_square')
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.create_subscription(Bool, 'estop', self.handle_estop, 10)
-        self.run_loop_thread = Thread(target=self.run_loop)
-        self.run_loop_thread.start()
+        self.done_pub = self.create_publisher(String, '/state_done', 10)
+        self.state_sub = self.create_subscription(String, '/fsm_state', self.state_callback, 10)
 
+        self.create_timer(0.1, self.run_loop)
+        self.turns_executed = 0
+        self.executing_turn = False
+        self.side_length = 1      # the length in meters of a square side
+        self.time_per_side = 5.0    # duration in seconds to drive the square side
+        self.time_per_turn = 2.0    # duration in seconds to turn 90 degrees
+        # start_time_of_segment indicates when a particular part of the square was
+        # started (e.g., a straight segment or a turn)
+        self.start_time_of_segment = None   
 
-    def handle_estop(self, msg):
-        """Handles messages received on the estop topic.
-
-        Args:
-            msg (std_msgs.msg.Bool): the message that takes value true if we
-            estop and false otherwise.
-        """ 
-        if msg.data:
-            self.e_stop.set()
-            self.drive(linear=0.0, angular=0.0)
-
+    def state_callback(self, msg):
+        self.active = (msg.data == self.STATE_NAME)
+    
     def run_loop(self):
-        """Executes the main logic for driving the square.  This function does
-        not return until the square is finished or the estop is pressed.
-        """
-        # the first message on the publisher is often missed
-        self.drive(0.0, 0.0)
-        sleep(1)
-        print(goal_reached)
-        for _ in range(4):
-            if not self.e_stop.is_set():
-                print("driving forward")
-                self.drive_forward(1)
-            if not self.e_stop.is_set():
-                print("turning left")
-                self.turn_left()
-        print('done with run loop')
-
-    def drive(self, linear, angular):
-        """Drive with the specified linear and angular velocity.
-
-        Args:
-            linear (_type_): the linear velocity in m/s
-            angular (_type_): the angular velocity in radians/s
-        """        
+        """ In the run_loop we are essentially implementing what's known as a finite-state
+            machine.  That is, our robot code is in a particular state (in this case defined
+            by whether or not we are turning and how many sides we've traversed thus far.
+            
+            Our run loop does the following things:
+              1. if we haven't yet marked the start time of the segment, we do so by grabbing the current time
+              2. we compute the desired time for the particular move we are executing (this will be our criteria to change state)
+              3. we check to see if we are done with our current segment and should move onto the next state
+                 -if we are done-
+                    - Transition to the next state by switching from a turn to a straight segment (or vice versa)
+                    - Reset the start time variable of the segment
+                    - set the desired velocities to 0 (so we stop in between each segment)
+                 -if we are not done-
+                    - Compute the appropriate velocity command based on the state
+                4. publish the velocity command 
+            """
+        if self.start_time_of_segment is None:
+            self.start_time_of_segment = self.get_clock().now()
         msg = Twist()
-        msg.linear.x = linear
-        msg.angular.z = angular
-        self.vel_pub.publish(msg)
+        if self.executing_turn:
+            segment_duration = self.time_per_turn
+        else:
+            segment_duration = self.time_per_side
 
-    def turn_left(self):
-        """Execute a 90 degree left turn
-        """
-        angular_vel = 0.3
-        if not self.e_stop.is_set():
-            self.drive(linear=0.0, angular=angular_vel)
-            sleep(math.pi / angular_vel / 2)
-            self.drive(linear=0.0, angular=0.0)
-
-    def drive_forward(self, distance):
-        """Drive straight for the specified distance.
-
-        Args:
-            distance (_type_): the distance to drive forward.  Only positive
-            values are supported.
-        """
-        forward_vel = 0.1
-        if not self.e_stop.is_set():
-            self.drive(linear=forward_vel, angular=0.0)
-        sleep(distance / forward_vel)
-        self.drive(linear=0.0, angular=0.0)
+        # check to see if we are done with the segment
+        # here, I use self.get_clock().now() which is better than using time.time() since it works
+        # equally well with simulator or wall clock time (e.g., the simulator might not run
+        # at real-time).  You are totally fine using time.time(), but I wanted to show this.
+        if self.get_clock().now() - self.start_time_of_segment > rclpy.time.Duration(seconds=segment_duration):
+            if self.executing_turn:
+                self.turns_executed += 1
+            # toggle the executing_turn Boolean (turn to not turn or vice versa)
+            self.executing_turn = not self.executing_turn
+            self.start_time_of_segment = None
+            print(self.executing_turn, self.turns_executed)
+            # transition to next segment, don't change msg so we execute a stop
+        else:
+            if self.executing_turn:
+                # we are trying to turn pi/2 radians in a particular amount of time
+                # from this we can get the angular velocity
+                msg.angular.z = (pi / 2) / segment_duration
+            else:
+                msg.linear.x = self.side_length / segment_duration
+        self.vel_pub.publish(msg) 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DrawSquare()
+    node = DriveSquareSample1()
     rclpy.spin(node)
-    node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
