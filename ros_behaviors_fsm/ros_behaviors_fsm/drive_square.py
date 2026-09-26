e6d0d0efe69422f98b29a7df6535a00b90b8160d
Smart-coder872 <mmiller4328@gmail.com>
2026-09-25 21:50:01 -0400

    Updates to drive square

--- ros_behaviors_fsm/ros_behaviors_fsm/drive_square.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/drive_square.py
@@ -1,36 +1,31 @@
+"""
+Draw Square
+--------
+This node encapsulates implements a simple time-based approach to driving the
+robot in a square.  The system makes use of a a special ``estop`` topic that
+can trigger the robot to automatically stop when the value is true is received
+on that topic.
+"""
 import rclpy
 from rclpy.node import Node
 from threading import Thread, Event
 from time import sleep
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
 from std_msgs.msg import Bool
 import math
 
 class DrawSquare(Node):
-    """A class for a square drawing node. This node subscribes to the estop topic and publishes to a cmd_vel topic.
+    """A class that implements a node to pilot a robot in a square.
     """
 
     def __init__(self):
-        super().__init__('draw_square')
+        super().__init__('draw_square_with_estop')
         self.e_stop = Event()
         # create a thread to handle long-running component
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.create_publisher(Bool, 'finished', 10) #msg to tell fsm that task is finished
-
         self.create_subscription(Bool, 'estop', self.handle_estop, 10)
-        self.create_subscription(Odometry, 'odom', self.handle_odom, 10)
-
         self.run_loop_thread = Thread(target=self.run_loop)
         self.run_loop_thread.start()
-
-        self.x_position_distance = 1.0 #meters
-        self.y_position_distance = 0.0 #meters
-        self.z_position_distance = 0.0 #meters
-        self.x_orientation_distance = 0.0 #degrees
-        self.y_orientation_distance = 0.0 #degrees
-        self.z_orientation_distance = 90 #degrees
 
 
     def handle_estop(self, msg):
@@ -44,62 +39,18 @@ class DrawSquare(Node):
             self.e_stop.set()
             self.drive(linear=0.0, angular=0.0)
 
-    def handle_odom(self, msg):
-        """Handles messages received on the odom topic.
-
-        Args:
-            msg (nav_msgs.msg.Odometry): the message that contains the odometry
-            information.
-        """
-        self.x = msg.pose.pose.position.x
-        self.y = msg.pose.pose.position.y
-        self.z = msg.pose.pose.position.z 
-        self.qx = msg.pose.pose.orientation.x
-        self.qy = msg.pose.pose.orientation.y
-        self.qz = msg.pose.pose.orientation.z
-
-        self.get_logger().info(f"odom: x={self.x}, y={self.y}, qx={self.qx}, qy={self.qy}, qz={self.qz}")
-
-    def distance_goal(self, position, orientation):
-        """Calculates the desired distance to a goal position and orientation.
-
-        Args:
-            position (tuple): the goal position as a tuple (x, y, z)
-            orientation (tuple): the goal orientation as a tuple (qx, qy, qz)
-        """
-        current_position = (self.x, self.y, self.z)
-        current_orientation = (self.qx, self.qy, self.qz)
-
-        new_position = []
-        for i in position:
-            desired_position = position[i] - current_position[i]
-            new_position.append(desired_position)
-
-        new_orientation = []
-        for i in orientation:
-            desired_orientation = orientation[i] - current_orientation[i]
-            new_orientation.append(desired_orientation)
-
-        return new_position, new_orientation
-
     def run_loop(self):
         """Executes the main logic for driving the square.  This function does
         not return until the square is finished or the estop is pressed.
         """
-        goal_reached = self.distance_goal((self.x_position_distance, 
-                                           self.y_position_distance, 
-                                           self.z_position_distance), 
-                                          (self.x_orientation_distance, 
-                                           self.y_orientation_distance, 
-                                           self.z_orientation_distance))
         # the first message on the publisher is often missed
         self.drive(0.0, 0.0)
         sleep(1)
         for _ in range(4):
-            while not self.e_stop.is_set() and not goal_reached:
-
+            if not self.e_stop.is_set():
                 print("driving forward")
-                self.drive_forward(0.5)
+                self.drive_forward(1)
+            if not self.e_stop.is_set():
                 print("turning left")
                 self.turn_left()
         print('done with run loop')
@@ -120,20 +71,21 @@ class DrawSquare(Node):
         """Execute a 90 degree left turn
         """
         angular_vel = 0.3
-        self.drive(linear=0.0, angular=angular_vel)
-        sleep(math.pi / angular_vel / 2)
-        self.drive(linear=0.0, angular=0.0)
+        if not self.e_stop.is_set():
+            self.drive(linear=0.0, angular=angular_vel)
+            sleep(math.pi / angular_vel / 2)
+            self.drive(linear=0.0, angular=0.0)
 
     def drive_forward(self, distance):
-        """Drive straight for the spefcified distance.
+        """Drive straight for the specified distance.
 
         Args:
             distance (_type_): the distance to drive forward.  Only positive
             values are supported.
         """
         forward_vel = 0.1
-
-        self.drive(linear=forward_vel, angular=0.0)
+        if not self.e_stop.is_set():
+            self.drive(linear=forward_vel, angular=0.0)
         sleep(distance / forward_vel)
         self.drive(linear=0.0, angular=0.0)
 
--- ros_behaviors_fsm/ros_behaviors_fsm/finite_state_machine.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/finite_state_machine.py
@@ -12,6 +12,7 @@ class FiniteStateMachine(Node):
         # Create a state that stores the current state of the FSM
         self.current_state = "IDLE"
 
+        # Recieves msgs from drive_square to indicate when behavior is finished
         self.create_subscription(Bool, "finished", self.handle_finished, 10)
 
     def handle_finished(self, msg):
@@ -20,16 +21,20 @@ class FiniteStateMachine(Node):
             self.get_logger().info("Drive square finished, transitioning to IDLE state.")
             self.current_state = "IDLE"
 
+    def handle_state(self, state):
+        if state == 0:
+            self.current_state = "IDLE"
+            self.get_logger().info("Transitioning to IDLE state.")
+        elif state == 1:
+            self.current_state = "DRIVE_SQUARE"
+            self.get_logger().info("Transitioning to DRIVE_SQUARE state.")
+        elif state == 2:
+            self.current_state = "WALL_FOLLOWER"
+            self.get_logger().info("Transitioning to WALL_FOLLOWER state.")
+        elif state == 3:
+            self.current_state = "DANCE"
+            self.get_logger().info("Transitioning to DANCE state.")
+
     def run_loop(self):
         # Main loop for the FSM. This function will be called repeatedly to check the current state and take appropriate actions.
-        if self.current_state == "IDLE":
-            #let user choose a behavior to run
-            pass
-        elif self.current_state == "DRIVE_SQUARE":
-            # Start the drive_square behavior
-            self.get_logger().info("Starting drive square behavior.")
-
-        elif self.current_state == "WALL_FOLLOWER":
-            # Start the wall follower behavior
-            self.get_logger().info("Starting wall follower behavior.")
-            
+        pass
